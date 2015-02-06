9bdccbcf6fe26e23f8738dfc3015069eb82cb789
Tom Kerkhove <kerkhove.tom@gmail.com>
2015-02-06 12:09:48 +0100

    Add template

---
+++ README.md
@@ -0,0 +1,6 @@
+Gen. II Kinect for Windows – First look at Expressions
+==============
+Template for the G2K Expressions overview that illustrate how easily you can access the expressions.
+
+![K4W logo](http://www.kinectingforwindows.com/wp-content/themes/twentyten/images/headers/logo.jpg)
+
---
+++ src/K4W.Expressions/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+
+namespace K4W.Expressions
+{
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Instance of Kinect sensor
+        /// </summary>
+        private KinectSensor _kinect;
+
+        /// <summary>
+        /// Default CTOR
+        /// </summary>
+        public MainWindow()
+        {
+            // Initialize Components
+            InitializeComponent();
+
+            // Initialize Kinect
+            InitializeKinect();
+        }
+
+        /// <summary>
+        /// Initialize Kinect
+        /// </summary>
+        private void InitializeKinect()
+        {
+            // Get Kinect sensor
+            _kinect = KinectSensor.GetDefault();
+
+            if (_kinect == null) return;
+
+            // Initialize Camera
+            InitializeCamera();
+
+            // Start receiving
+            _kinect.Open();
+        }
+
+        #region CAMERA
+        /// <summary>
+        /// Color WriteableBitmap linked to our UI
+        /// </summary>
+        private WriteableBitmap _colorBitmap = null;
+        /// <summary>
+        /// Array of color pixels
+        /// </summary>
+        private byte[] _colorPixels = null;
+
+        /// <summary>
+        /// FrameReader for our coloroutput
+        /// </summary>
+        private ColorFrameReader _colorReader = null;
+        /// <summary>
+        /// Size fo the RGB pixel in bitmap
+        /// </summary>
+        private readonly int _bytePerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private void InitializeCamera()
+        {
+            if (_kinect == null) return;
+
+            // Get frame description for the color output
+            FrameDescription desc = _kinect.ColorFrameSource.FrameDescription;
+
+            // Get the framereader for Color
+            _colorReader = _kinect.ColorFrameSource.OpenReader();
+
+            // Allocate pixel array
+            _colorPixels = new byte[desc.Width * desc.Height * _bytePerPixel];
+
+            // Create new WriteableBitmap
+            _colorBitmap = new WriteableBitmap(desc.Width, desc.Height, 96, 96, PixelFormats.Bgr32, null);
+
+            // Link WBMP to UI
+            CameraImage.Source = _colorBitmap;
+
+            // Hook-up event
+            _colorReader.FrameArrived += OnColorFrameArrived;
+        }
+        /// <summary>
+        /// Process color frames & show in UI
+        /// </summary>
+        private void OnColorFrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            // Get the reference to the color frame
+            ColorFrameReference colorRef = e.FrameReference;
+
+            if (colorRef == null) return;
+
+            // Acquire frame for specific reference
+            ColorFrame frame = colorRef.AcquireFrame();
+
+            // It's possible that we skipped a frame or it is already gone
+            if (frame == null) return;
+
+            using (frame)
+            {
+                // Get frame description
+                FrameDescription frameDesc = frame.FrameDescription;
+
+                // Check if width/height matches
+                if (frameDesc.Width == _colorBitmap.PixelWidth && frameDesc.Height == _colorBitmap.PixelHeight)
+                {
+                    // Copy data to array based on image format
+                    if (frame.RawColorImageFormat == ColorImageFormat.Bgra)
+                    {
+                        frame.CopyRawFrameDataToArray(_colorPixels);
+                    }
+                    else frame.CopyConvertedFrameDataToArray(_colorPixels, ColorImageFormat.Bgra);
+
+                    // Copy output to bitmap
+                    _colorBitmap.WritePixels(
+                            new Int32Rect(0, 0, frameDesc.Width, frameDesc.Height),
+                            _colorPixels,
+                            frameDesc.Width * _bytePerPixel,
+                            0);
+                }
+            }
+        }
+        #endregion CAMERA
+    }
+}
